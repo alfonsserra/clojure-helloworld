9dd94954b05a4c9f2497f6ac17ebf676a1a5a457
Alfons Serra <alfonsoserra@gmail.com>
2020-01-09 07:20:41 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject helloworld "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple Hello world"
+  :url "https://github.com/alfonsserra/clojure-helloworld"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
