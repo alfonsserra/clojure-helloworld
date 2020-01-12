03cbc1b239f65750fd9cd9c15ab7cea4bb78c47d
alfonsserra <alfonsoserra@gmail.com>
2020-01-12 19:32:05 +0100

    Manage an array

--- src/helloworld/core.clj
+++ src/helloworld/core.clj
@@ -1,11 +1,15 @@
 (ns helloworld.core)
 
+(defn commasepared
+  [names]
+  (clojure.string/join ", " (map #(str % ) names)))
+
 (defn greet
-  "Say hello to a person"
-  [name]
-  (println "Hello," name))
+  "Say hello"
+  [names]
+  (println "Hello" (commasepared names)))
 
 (defn -main
   "Main"
   []
-  (greet "Maria"))
+  (greet ["Maria" "Peter" "Josh"]))
